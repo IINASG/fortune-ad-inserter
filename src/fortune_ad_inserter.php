8ade85a901ce10c74d76cc06e110d036c5de57ca
atamiso <atamini@gmail.com>
2024-11-16 09:35:50 +0900

    fix: first commit

--- src/fortune_ad_inserter.php
+++ src/fortune_ad_inserter.php
@@ -10,7 +10,7 @@
 
 if ( !defined( 'ABSPATH' ) ) {
     exit;
-} // WordPressが存在する場合のみ実行
+}
 
 class FortuneAdInserter
 {
@@ -112,37 +112,7 @@ class FortuneAdInserter
     // 広告設定ページのHTML
     public function settings_page_html()
     {
-        ?>
-        <div class="wrap">
-            <h1>フォーチュンアド設定</h1>
-            <form method="post" action="options.php">
-                <?php settings_fields( $this->option_name ); ?>
-                <?php $options = get_option( $this->option_name ); ?>
-
-                <table class="form-table">
-                    <tr>
-                        <th scope="row">広告コード</th>
-                        <td>
-                            <label>
-                                <input type="text" name="<?php echo $this->option_name; ?>[ad_code]" value="<?php echo isset( $options['ad_code'] ) ? esc_attr( $options['ad_code'] ) : ''; ?>"/>
-                            </label>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">広告表示</th>
-                        <td>
-                            <label>
-                                <input type="checkbox" name="<?php echo $this->option_name; ?>[enable_ad]" value="1" <?php checked( 1, $options['enable_ad'] ?? 0 ); ?> />
-                            </label>
-                            有効化
-                        </td>
-                    </tr>
-                </table>
-                
-                <?php submit_button(); ?>
-            </form>
-        </div>
-        <?php
+        include __DIR__ . '/template/setting.php';
     }
     
     // 広告スクリプトをヘッダーに挿入
@@ -167,4 +137,3 @@ class FortuneAdInserter
 
 // プラグイン初期化
 new FortuneAdInserter();
-?>
---
+++ src/template/setting.php
@@ -0,0 +1,29 @@
+<div class="wrap">
+    <h1>フォーチュンアド設定</h1>
+    <form method="post" action="options.php">
+        <?php settings_fields( $this->option_name ); ?>
+        <?php $options = get_option( $this->option_name ); ?>
+
+        <table class="form-table">
+            <tr>
+                <th scope="row">広告コード</th>
+                <td>
+                    <label>
+                        <input type="text" name="<?php echo $this->option_name; ?>[ad_code]" value="<?php echo isset( $options['ad_code'] ) ? esc_attr( $options['ad_code'] ) : ''; ?>"/>
+                    </label>
+                </td>
+            </tr>
+            <tr>
+                <th scope="row">広告表示</th>
+                <td>
+                    <label>
+                        <input type="checkbox" name="<?php echo $this->option_name; ?>[enable_ad]" value="1" <?php checked( 1, $options['enable_ad'] ?? 0 ); ?> />
+                    </label>
+                    有効化
+                </td>
+            </tr>
+        </table>
+        
+        <?php submit_button(); ?>
+    </form>
+</div>
