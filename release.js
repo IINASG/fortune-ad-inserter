063521b270dd95e00b60cb56dc6c168d8c6daf66
atamiso <atamini@gmail.com>
2024-11-18 07:15:32 +0900

    fix: GitHub Actionsの調整

--- release.js
+++ release.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const zip = require('zip-a-folder').zip;
 const fs = require('fs');
+const marked = require('marked');
 const jsonFilename = './api/fortune-ad-inserter.json';
 const phpFilename = './src/fortune_ad_inserter.php';
 const json = JSON.parse(fs.readFileSync(jsonFilename, 'utf8'));
 const php = fs.readFileSync(phpFilename, 'utf8') ;
+let changelog = fs.readFileSync('./CHANGELOG.md', 'utf8')
+                            .replace(/### \[[0-9]+\.[0-9]+\.[0-9]+].*[\r\n]+/,'')
+                            .replaceAll(/ \(\[[0-9a-f]{7}].+\)\)/gi,'');
 
 if( ! process.env.NEW_VERSION ) {
   throw new Error('env NEW_VERSION not set')
@@ -12,7 +16,7 @@ if( ! process.env.NEW_VERSION ) {
 
 json.new_version = process.env.NEW_VERSION ;
 json.last_updated = process.env.LAST_UPDATE || new Date().toLocaleDateString('sv-SE') ;
-json.changelog = process.env.CHANGELOG ;
+json.changelog = marked.parse(changelog);
 
 fs.writeFileSync( jsonFilename , JSON.stringify(json));
 fs.writeFileSync( phpFilename , php.replace(/Version:.*[0-9]+\.[0-9]+\.[0-9]+/, 'Version: ' + json.new_version ) );
